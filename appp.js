0701d422a3933a0666a3601783f7f00ccf65ce3d
murka91 <166361058+murka91@users.noreply.github.com>
2024-06-20 21:59:32 +0600

    Add files via upload

---
+++ appp.js
@@ -0,0 +1,42 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+    /* Fixed Header */
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    /* ДАТА КАЛЕНДАРЯ*/
+    var today = new Date().toISOString().split('T')[0];
+document.getElementsByName("date-control")[0].setAttribute('min', today);
+    });
+
+   
